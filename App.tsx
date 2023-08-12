4d0bf6c84602582bf66b2665316f34a2603cae2c
rajkumar-sendhav <131992728+rajkumar-sendhav@users.noreply.github.com>
2023-08-12 11:12:29 +0530

    3rd commit

--- App.tsx
+++ App.tsx
@@ -1,37 +1,63 @@
-import * as React from 'react';
-import 'react-native-gesture-handler';
-import {Button, View} from 'react-native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+/* eslint-disable react/no-unstable-nested-components */
+// React Navigation v6 - Drawer Navigation - Side Menu
+// https://reactnavigation.org/docs/drawer-navigator
+
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-
-function HomeScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import ScreenA from './src/Demo/Demo13/ScreenA';
+import ScreenB from './src/Demo/Demo13/ScreenB';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const Drawer = createDrawerNavigator();
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator useLegacyImplementation initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      <Drawer.Navigator
+        screenOptions={{
+          headerShown: true,
+          swipeEnabled: true,
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#0080ff',
+          },
+          headerTintColor: '#ffffff',
+          headerTitleStyle: {
+            fontSize: 25,
+            fontWeight: 'bold',
+          },
+        }}>
+        <Drawer.Screen
+          name="Home"
+          component={ScreenA}
+          options={{
+            drawerIcon: ({focused}) => (
+              <FontAwesome5
+                name="autoprefixer"
+                size={focused ? 25 : 20}
+                color={focused ? '#007AFF' : '#999999'}
+              />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Profile"
+          component={ScreenB}
+          options={{
+            title: 'ScreenB Title',
+            drawerIcon: ({focused}) => (
+              <FontAwesome5
+                name="btc"
+                size={focused ? 25 : 20}
+                color={focused ? '#007AFF' : '#999999'}
+              />
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+export default App;
